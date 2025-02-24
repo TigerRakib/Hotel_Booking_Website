0e36d504e4074fed1cc74d873beb20a496e0197c
Nazrul Islam Rakib <88043438+TigerRakib@users.noreply.github.com>
2025-02-24 10:00:33 +0600

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,12 @@
+#!/bin/bash
+
+# Ensure Python is installed
+export PATH=$HOME/.local/bin:$PATH
+
+# Install dependencies
+python3 -m ensurepip --default-pip
+pip install --upgrade pip
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Collect static files
+python3 manage.py collectstatic --noinput
